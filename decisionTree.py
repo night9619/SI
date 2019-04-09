e3f1aee66e357b99ab6a7147d5db299a213cc97a
night9619 <32632458+night9619@users.noreply.github.com>
2019-04-09 10:09:34 +0200

    wyswieta wszystko

--- decisionTree.py
+++ decisionTree.py
@@ -5,29 +5,39 @@ with open('cw 1/test.txt', 'r') as f:
 
 lista = [el.split(',') for el in element]
 
+#kazda kolumna tworzy osobna liste
 poKolumnach = []
 for kolumna in range(len(lista[0])):
     kol = []
     for element in lista:
         kol.append(element[kolumna])
     poKolumnach.append(kol)
-
+    
+#kolumna decyzji
 decyzja = poKolumnach[-1]
+
+#liczba rekordow
 T = len(poKolumnach[-1])
+
+#liczba atrybutów (kolumn)
 lAtrybutow= len(lista[0])-1
+
+#unikalne wartosci decyzji
 unikalneDecyzje = sorted(set([el for el in decyzja]))
 
+#unikalne wartosci atrybutów (unikalne wart w kazdej kolumnie)
 unikalne = {}
 for nrK in range(len(lista[0])):
     unikalne[nrK+1] = list(set([el for el in poKolumnach[nrK]]))
 
-
+#liczba wart w danej kolumnie (w danym atrybucie)
 ileWartosciWKolumnie = {}
 for nrKolumny in range(len(poKolumnach)):
     kolumna = poKolumnach[nrKolumny]
     wartosci = {i for i in kolumna}
     ileWartosciWKolumnie[nrKolumny+1] = len(wartosci)
 
+#podusmowanie klasy, ile jest danych wartosci dla danego atrybutu
 sumaKlasy = {}
 for nrK, kolumna in enumerate(poKolumnach):
     k = {}
@@ -36,6 +46,7 @@ for nrK, kolumna in enumerate(poKolumnach):
             k[u] = kolumna.count(u)
     sumaKlasy[nrK+1] = k
     
+#jakie sa dacyzje dla danych wart w klasach
 podsumowanieKlas = { }
 for nr in range(len(poKolumnach)):
     dlaKolumny = {}
@@ -55,19 +66,12 @@ def I(P):
     wynik = (-1)*sum([p*log2(p) for p in P if p!=0])
     return wynik
 
-##def Info(X,T):
-##    wynik = I(X)*(Ti/T)
-##    return wynik
-
 podsumowanieDecyzji = sumaKlasy[len(poKolumnach)]
 zbior = [podsumowanieDecyzji[a]/len(decyzja) for a in podsumowanieDecyzji]
 
 def Informacje(numer):
-    #maxA=0
-    #maxGain=0
     info=0
     split=0
-    #gainRatio=0
     print("\n")
     print("A"+str(numer))
     print(sumaKlasy[numer])
@@ -82,16 +86,15 @@ def Informacje(numer):
         Ti = sum(liczebnosc)
         PP = [C[w][a]/Ti if Ti!=0 else "0" for a in C[w]]
         print("\t",PP)
-        #print("\t",Info(PP,T))
         print("\t",I(PP),'*', Ti/T)
         info += I(PP) * (Ti/T)
-        #split += ((Ti/T) * (-1)*sum([Ti/T * log2(Ti/T) for p in PP if p!=0]))
         split += ((-1)*(Ti/T)* log2(Ti/T))
                   
         print("\t", info)
         
     gain = I(zbior)-info
-    gainRatio = gain/split
+    if split > 0:
+        gainRatio = gain/split
     
     Info[numer] = info
     Gain[numer] = gain
@@ -109,21 +112,21 @@ for nr in range(len(poKolumnach)-1):
 
 print("\n")
 for nr in range(len(poKolumnach)-1):
-    #print(f"Info(A{str(nr+1)},T)= {Info(1,T)}   \t Gain(A{str(nr+1)},T)= {Gain[nr+1]}")
     print(f"Info(A{str(nr+1)},T)= {Info[nr+1]}   \t Gain(A{str(nr+1)},T)= {Gain[nr+1]}")
 
+#wyznacza max Gain
+for a in Gain:
+    if Gain[a]==max(Gain.values()):
+        maxA=a
+        
+#wyznacza max Gain i usuwa wczesniejsze atrybuty        
 def maxAtryb(atryb):
-    #Gain.remove(atryb)
     if atryb in Gain:
         del Gain[atryb]
     for a in Gain:
         if Gain[a]==max(Gain.values()):
             maxA=a
             return maxA
-
-for a in Gain:
-    if Gain[a]==max(Gain.values()):
-        maxA=a
 
 print("\nWybrany atrybut A{}, bo w tym przypadku najwyższa jest wartość Gain: {}".format(maxA,max(Gain.values())))
 
@@ -132,42 +135,41 @@ for nr in range(len(poKolumnach)-1):
     print(f"Split(D{str(nr+1)},T)= {Split[nr+1]}   \t Gain Ratio(D{str(nr+1)},T)= {GainRatio[nr+1]}")
 
 def szukaj(atryb):
-#     if atryb>lAtrybutow:
-#         print("Koniec drzewa")
-#     else:
-        C = [ podsumowanieKlas[atryb][x] for x in podsumowanieKlas[atryb] ]
-        for w in range(ileWartosciWKolumnie[atryb]):
-            print("\n")
-            print(f"\tA{atryb} =",unikalne[atryb][w],":")       
-            Ti = sum([C[w][a] for a in C[w]])
-            PP = [C[w][a]/Ti for a in C[w] if Ti!=0]
-            #print("\t",PP)
-            #print("\t",I(PP))
+    C = [ podsumowanieKlas[atryb][x] for x in podsumowanieKlas[atryb] ] 
+    for w in range(ileWartosciWKolumnie[atryb]):
+        tab="\t"*atryb
+        
+        #print(f"{tab}A{atryb} =",unikalne[atryb][w],":")       
+        Ti = sum([C[w][a] for a in C[w]])
+        PP = [C[w][a]/Ti for a in C[w] if Ti!=0]
+        
+        def dec():
             for a in C[w]:
-                if I(PP) == 0 and C[w][a]!=0:
-                    print("\t\t"+a+f"({C[w][a]})")
-                elif C[w][a]==0:
-                    continue
-                else:
-                    
-                    if atryb>=lAtrybutow:
-                        print("\t\t"+a+f"({C[w][a]})")
-                    else:
-                        print("dalej")
-                        noweA = maxAtryb(atryb)
-                        print(noweA)
-                        print("---->")
-                        szukaj(noweA)
+                if a!=0:
+                    k = a+f"({C[w][a]})"
+            return k
 
+        for a in C[w]:            
+            if I(PP) == 0 and C[w][a]!=0:
+                print(f"{tab}\tA{atryb} =",unikalne[atryb][w],":", a) 
+                #print(f"{tab}\t|--->"+a+f"({C[w][a]})")
+            elif C[w][a]==0:
+                continue
+            else:
+                if atryb>=lAtrybutow:
+                    print(f"{tab}\tA{atryb} =",unikalne[atryb][w],":",a) 
+                   # print(f"{tab}\t|--->"+a+f"({C[w][a]})")
+                else:
+                    print(f"{tab}A{atryb} =",unikalne[atryb][w],":") 
+                    print(f"{tab}|-------------->")
+                    
+                    noweA = maxAtryb(atryb) 
+                    if noweA!=atryb:
+                        szukaj(noweA)
 
     
 def budujDrzewo():
-    print("Drzwo decyzyjne dla {0} przypadków ({1} atrybutów)".format(T,lAtrybutow))
-    szukaj(maxA)
-    #print(maxA)
-    #podzbiory = [ podsumowanieKlas[maxA][x] for x in podsumowanieKlas[maxA] ]
-    #print(podzbiory)
-
-    
+    print("\nDrzwo decyzyjne dla {0} przypadków ({1} atrybutów)".format(T,lAtrybutow))
+    szukaj(maxA) 
     
 budujDrzewo()
